44c410057b493242c48d2693d493dd02fb01a64c
cha_jae_geon <jaegun920@naver.com>
2022-12-20 13:06:08 +0900

    주소록 저장 프로그램 수정 commit

--- AddressBook/src/arrayLIstEx/AddressBookEx.java
+++ AddressBook/src/arrayLIstEx/AddressBookEx.java
@@ -14,30 +14,77 @@ public class AddressBookEx {
 			String code = scan.next();
 			switch(code) {
 				case "1": input(scan, addressBook); break;
-				case "2": searchAll(); break;
-				case "3": searchName(); break;
-				case "4": delete(); break;
+				case "2": searchAll(addressBook); break;
+				case "3": searchName(scan, addressBook); break;
+				case "4": delete(scan, addressBook); break;
 				case "x": break;
 				default: System.out.println("입력 구분을 잘못 입력하셨습니다!!");
 			}
 			if (code.equals("x")) {
 				System.out.println("**** 프로그램을 이용해 주셔서 감사합니다. ****");
-				ProgramInfo.print("\n주소록 프로그램", "차재건", 1.0);
 				break;
 			}
 		}
 	}
 
-	private static void delete() {
+	private static void delete(Scanner scan, ArrayList <Address> addressBook) {
 		System.out.println("*** 주소 삭제 처리 ***");
+		if(addressBook.size() <= 0) {
+			System.out.println("등록된 연락처가 없습니다.");
+		} else {
+			System.out.println("이름 입력: ");
+			String name = scan.next();
+			int cnt = 0;
+			System.out.println("** 등록 번호 내역 **");
+			for (int i = 0; i < addressBook.size(); i++) {
+				if (name.equals(addressBook.get(i).getName())) {
+					cnt ++;
+					System.out.println(i +") " + ": " + addressBook.get(i).getPhone());
+				}
+			}
+			if(cnt == 0) System.out.println("**** 등록된 연락처가 없습니다. ****");
+			else {
+				System.out.print("삭제할 목록번호 #) 입력: ");
+				String num = scan.next();
+				System.out.println("정말 삭제하시겠습니까? (y/n)");
+				String check = scan.next();
+				if (check.equals("y")) {
+					// ArrayList -> key로(첫 번째 거)도, 주소로도 지우게 할 수 있다.
+					addressBook.remove(Integer.parseInt(num));
+					System.out.println("삭제되었습니다.");
+				} else System.out.println("취소 되었습니다...");
+			}
+		}
 	}
 
-	private static void searchName() {
+	private static void searchName(Scanner scan, ArrayList<Address> addressBook) {
 		System.out.println("*** 이름으로 주소 조회 ***");
+		if(addressBook.size() <= 0) {
+			System.out.println("등록된 연락처가 없습니다.");
+		} else {
+			System.out.print("이름 입력: ");
+			String name = scan.next();
+			int cnt = 0;
+			for (int i = 0; i < addressBook.size(); i++) {
+				if (name.equals(addressBook.get(i).getName())) {
+					System.out.println(name + " : " + addressBook.get(i).getPhone());
+					cnt ++;
+				}
+			}
+			if (cnt == 0) System.out.println("등록된 연락처가 없습니다.");
+		}
 	}
 
-	private static void searchAll() {
+	private static void searchAll(ArrayList<Address> addressBook) {
 		System.out.println("*** 전체 주소 조회 ***");
+		if(addressBook.size() <= 0) {
+			System.out.println("등록된 연락처가 없습니다.");
+		}
+		for (int i = 0; i < addressBook.size(); i++) {
+			String a_name = addressBook.get(i).getName();
+			String a_phone = addressBook.get(i).getPhone();
+			System.out.println((i+1) + ") " + a_name + " " + a_phone);
+		}
 	}
 
 	private static void input(Scanner scan, ArrayList<Address> addressBook) {
@@ -52,4 +99,5 @@ public class AddressBookEx {
 		String a_phone = addressBook.get(size).getPhone();
 		System.out.println("등록 완료 ==> " + a_name + " " + a_phone);
 	}
+	
 }
